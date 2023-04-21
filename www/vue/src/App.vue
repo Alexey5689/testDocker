258ab5cd4635ce6f0463b2cae16fcaf242c18041
Alex5689 <alex.dya4encko89@yandex.ru>
2023-04-21 11:54:43 +0500

    1commit

---
+++ www/vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="addlike">like</button>
+    <button @click="adddislike">dislike</button>
+    <div>
+      Колличество лайков <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Колличество дизлайков <strong>{{ dislikes }}</strong>
+    </div>
+
+    
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        likes:0,
+        dislikes:0,
+      }
+    },
+    methods:{
+      addlike(){
+        this.likes +=1;
+      },
+      adddislike(){
+        this.dislikes +=1;
+      }
+    }
+  }
+
+</script>
+<style>
+
+
+</style>
